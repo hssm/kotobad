9433a4dd8b8c3b13dd95faca0c2a93880ab7a93f
hssm <houssam.salem.au@gmail.com>
2025-07-08 13:06:54 +1000

    Add reply posts

--- poster.py
+++ poster.py
@@ -6,29 +6,38 @@ client.login('', '')
 
 def load_data():
     with open("wordlist.txt", "r") as f:
-        current = ""
+        post = [""]
+        reply = [""]
+        current = post
         for line in f:
             if line.startswith("---BEGIN"):
+                post[0] = ""
+                reply[0] = ""
+                current = post
+                continue
+            if line.startswith("---REPLY"):
+                current = reply
                 continue
             if line.startswith("---END"):
-                entries.append(current)
-                current = ""
+                entries.append((post[0], reply[0]))
+                current = None
                 continue
-            current += line
+            if current:
+                current[0] += line
 
 def post_next():
     with open('index', 'r') as f:
         index = int(f.read())
 
     index += 1
-    post_body = entries[index].strip()
+    post_body, reply_body = entries[index]
     post = client.send_post(post_body)
-    parent = models.create_strong_ref(post)
-
-    client.send_post(
-        text='reply post',
-        reply_to=models.AppBskyFeedPost.ReplyRef(parent=parent, root=parent)
-    )
+    if len(reply_body) > 0:
+        parent = models.create_strong_ref(post)
+        client.send_post(
+            text=reply_body,
+            reply_to=models.AppBskyFeedPost.ReplyRef(parent=parent, root=parent)
+        )
 
     with open("index", "w+") as f:
         f.write(str(index))
